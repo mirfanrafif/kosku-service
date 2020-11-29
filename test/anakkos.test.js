d0e3a08bbc00992bb5648ba0bf31717bfddb2d93
Irfan Rafif <mirfanrafif17@gmail.com>
2020-11-29 21:49:27 +0700

    sama nyoba test :)

---
+++ test/anakkos.test.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+const { expect } = require('chai');
+var chai = require('chai'), chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var app = 'http://localhost:3000';
+
+describe('anak kos', () => {
+  it('get anak kos data', async () => {
+    chai.request(app).get('/anakkos').end((err, res) => {
+      expect(res)
+      .to.be.json
+      .to.have.status(200);
+    })
+  })
+
+  it('get anak kos by id', () => {
+    chai.request(app).get('/anakkos/1').end((err, res) => {
+      expect(res)
+      .to.be.json
+      .to.have.status(200);
+    })
+  })
+
+  it('store data anak kos', () => {
+    chai.request(app).post('/anakkos').send({
+      nama: "Irfan Rafif",
+      asal: "Malang",
+      nohp: "02832383019"
+    }).end((err, res) => {
+      expect(res).to.be.json
+      .to.have.status(200)
+    })
+  })
+})
